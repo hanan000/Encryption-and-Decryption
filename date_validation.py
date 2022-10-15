6932d671b136aa7cc0be863c4eb5983b2da6d843
HananAbbas <hananwahab@outlook.com>
2022-10-15 23:19:36 +0300

    created an offline class to encode and decode the date.

--- date_validation.py
+++ date_validation.py
@@ -6,5 +6,18 @@ import datetime
 from datetime import datetime, date
 
 
+class OfflineValidation:
+    """OfflineValidation Class for determining whether the offline App is valid or not."""
 
-
+    def __init__(self, length, date, num):
+        """
+       Args:
+           length: is the length of the random string
+           date: the validation date of the assistant.
+           num: the number that will be added to encrypt the date.
+       """
+        self.length = length
+        self.date = date
+        self.num = num
+        self.random_string = self.get_random_string()
+        self.encrypt_the_date = self.encrypt_the_date()
